63a1687062170c16f6f8477fa90186a948dadb26
ezixd23 <ziad.elkarrabi@gmail.com>
2023-10-08 14:10:57 +0200

    happy path y invalid function

--- src/test/java/cat/tecnocampus/fgcstations/FgCstationsApplicationTests.java
+++ src/test/java/cat/tecnocampus/fgcstations/FgCstationsApplicationTests.java
@@ -3,8 +3,10 @@ package cat.tecnocampus.fgcstations;
 import cat.tecnocampus.fgcstations.application.DTOs.FriendsDTO;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.ErrorResponse;
@@ -12,17 +14,33 @@ import org.springframework.web.ErrorResponse;
 @SpringBootTest
 class FgCstationsApplicationTests {
 
+
     private TestRestTemplate restTemplate = new TestRestTemplate();
+
+    @LocalServerPort
+    private int port;
 
     @Test
     void contextLoads() {
     }
 
     @Test
-    public void testInvalidFriendUsername(){
+    public void testAddFriend_ValidUsername() {
+        String url = "http://localhost:" + port + "/api/users/friends";
+        FriendsDTO validFriend = new FriendsDTO();
+        validFriend.setUsername("validusername"); // Valid username (between 3 and 255 characters, lowercase)
+
+        ResponseEntity<Void> response = restTemplate.postForEntity(url, validFriend, Void.class);
+        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+
+    @Test
+    public void testAddFriend_InvalidUsername() {
+        String url = "http://localhost:" + port + "/api/users/friends";
         FriendsDTO invalidFriend = new FriendsDTO();
-        invalidFriend.setUsername("A");
-        ResponseEntity<ErrorResponse> response = restTemplate.postForEntity("http://localhost:8080/api/users/friends", invalidFriend, ErrorResponse.class);
+        invalidFriend.setUsername("ab"); // Invalid username (less than 3 characters, lowercase)
+
+        ResponseEntity<Void> response = restTemplate.postForEntity(url, invalidFriend, Void.class);
         Assertions.assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
     }
 }
