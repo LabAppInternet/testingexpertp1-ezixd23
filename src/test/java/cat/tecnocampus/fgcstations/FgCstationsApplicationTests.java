611e0bb402e711322fd7f0d844d8669232fa625c
ezixd23 <ziad.elkarrabi@gmail.com>
2023-10-09 12:28:47 +0200

    mock

--- src/test/java/cat/tecnocampus/fgcstations/FgCstationsApplicationTests.java
+++ src/test/java/cat/tecnocampus/fgcstations/FgCstationsApplicationTests.java
@@ -1,6 +1,7 @@
 package cat.tecnocampus.fgcstations;
 
 import cat.tecnocampus.fgcstations.application.DTOs.FriendsDTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,6 +10,7 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.client.RestTemplate;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -17,6 +19,28 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @SpringBootTest
 class FgCstationsApplicationTests {
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    FriendsDTO validF;
+    @Test
+    void testUsernameFriend() throws Exception{
+        validF = new FriendsDTO();
+        validF.setUsername("valid");
+        validF.setFriends();
+
+        String friend = """
+                    {"username": "valid",
+                     "friends": ["tina"] 
+                    }
+                    """;
+        }
+
+
+    /*
     @Autowired
     private RestTemplate restTemplate;
 
@@ -37,14 +61,5 @@ class FgCstationsApplicationTests {
         ResponseEntity<Void> response = restTemplate.postForEntity(url, validFriend, Void.class);
         assertEquals(HttpStatus.OK, response.getStatusCode());
     }
-
-    @Test
-    public void testAddFriend_InvalidUsername() {
-        String url = "http://localhost:" + port + "/api/users/friends";
-        FriendsDTO invalidFriend = new FriendsDTO();
-        invalidFriend.setUsername("ab"); // Invalid username (less than 3 characters, lowercase)
-
-        ResponseEntity<Void> response = restTemplate.postForEntity(url, invalidFriend, Void.class);
-        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
-    }
+    */
 }
